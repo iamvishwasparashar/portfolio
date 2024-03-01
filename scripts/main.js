2dfa1be7bd651c9617356a82d77efbf3939fc2f2
Vishwas Parashar <122075261+iamvishwasparashar@users.noreply.github.com>
2024-03-01 10:09:28 +0530

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,109 @@
+(function() {
+  "use strict";
+
+  window.addEventListener('load', () => {
+    on_page_load()
+  });
+
+  /**
+   * Function gets called when page is loaded.
+   */
+  function on_page_load() {
+    // Initialize On-scroll Animations
+    AOS.init({
+      anchorPlacement: 'top-left',
+      duration: 600,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false,
+      disable: 'mobile'
+    });
+  }
+
+  /**
+   * Navbar effects and scrolltop buttons upon scrolling
+   */
+  const navbar = document.getElementById('header-nav')
+  var body = document.getElementsByTagName("body")[0]
+  const scrollTop = document.getElementById('scrolltop')
+  window.onscroll = () => {
+    if (window.scrollY > 0) {
+      navbar.classList.add('fixed-top', 'shadow-sm')
+      body.style.paddingTop = navbar.offsetHeight + "px"
+      scrollTop.style.visibility = "visible";
+      scrollTop.style.opacity = 1;
+    } else {
+      navbar.classList.remove('fixed-top', 'shadow-sm')
+      body.style.paddingTop = "0px"
+      scrollTop.style.visibility = "hidden";
+      scrollTop.style.opacity = 0;
+    }
+  };
+
+  /**
+   * Masonry Grid
+   */
+  var elem = document.querySelector('.grid');
+  if(elem) {
+    imagesLoaded(elem, function() {
+      new Masonry(elem, {
+        itemSelector: '.grid-item',
+        percentPosition: true,
+        horizontalOrder: true
+      });
+    })
+  }
+
+  /**
+   * Big Picture Popup for images and videos
+   */
+   document.querySelectorAll("[data-bigpicture]").forEach((function(e) {
+     e.addEventListener("click", (function(t){
+       t.preventDefault();
+       const data =JSON.parse(e.dataset.bigpicture)
+       BigPicture({
+        el: t.target,
+        ...data
+      })
+     })
+    )
+  }))
+
+  /**
+   * Big Picture Popup for Photo Gallary
+   */
+   document.querySelectorAll(".bp-gallery a").forEach((function(e) {
+    var caption = e.querySelector('figcaption')
+    var img = e.querySelector('img')
+    // set the link present on the item to the caption in full view
+    img.dataset.caption = '<a class="link-light" target="_blank" href="' + e.href + '">' + caption.innerHTML + '</a>';
+    window.console.log(caption, img)
+     e.addEventListener("click", (function(t){
+       t.preventDefault();
+       BigPicture({
+        el: t.target,
+        gallery: '.bp-gallery',
+      })
+     })
+    )
+  }))
+
+  // Add your javascript here
+
+
+})();
+
+
+// Watson Assistant
+
+  window.watsonAssistantChatOptions = {
+    integrationID: "df8ca872-479a-4b3a-9e1f-112b5743ba8b", // The ID of this integration.
+    region: "au-syd", // The region your integration is hosted in.
+    serviceInstanceID: "1317983b-669c-4604-8072-10b587a37e66", // The ID of your service instance.
+    onLoad: async (instance) => { await instance.render(); }
+  };
+  setTimeout(function(){
+    const t=document.createElement('script');
+    t.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js";
+    document.head.appendChild(t);
+  });
